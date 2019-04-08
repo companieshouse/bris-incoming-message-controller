37371e1185624944b7e0c47365572274adf65bfe
Rafa <rgarcia@companieshouse.gov.uk>
2019-04-08 15:49:22 +0100

    Message content validation error

--- src/main/java/uk/gov/ch/bris/error/ErrorCode.java
+++ src/main/java/uk/gov/ch/bris/error/ErrorCode.java
@@ -32,7 +32,8 @@ public enum ErrorCode {
     ERR_BR_5104 ("Response message correlation error. Response message not correlated to any request.,Response message correlation error. Response message not correlated to any request.,The validation of the message failed"),
     ERR_BR_5105 ("Response/Request compliance error,The validation of the message failed"),
     ERR_BR_5106 ("BR message can not be deserialised to correct message type. ,The validation of the message failed"),
-
+    ERR_BR_5108 ("Message Container failed validation."),
+    
     ERR_BR_5200 ("The processing of the message failed"),
     ERR_BR_5201 ("BR unexpected internal (technical) error,The processing of the message failed (unexpected error)");
 
