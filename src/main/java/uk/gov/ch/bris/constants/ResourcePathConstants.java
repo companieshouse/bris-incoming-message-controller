ef114c9e1b0fd228a96748fd3e0c3ba738d7ca29
chrism84 <cmorgan@companieshouse.gov.uk>
2019-10-09 11:05:47 +0100

    Changed tabs to spaces

--- src/main/java/uk/gov/ch/bris/constants/ResourcePathConstants.java
+++ src/main/java/uk/gov/ch/bris/constants/ResourcePathConstants.java
@@ -37,23 +37,23 @@ public class ResourcePathConstants {
     public static final String ADD_BR_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-AddBusinessRegisterNotificationTemplate.xsd";
     public static final String REMOVE_BR_NOTIFICATION_SCHEMA ="xsd/bris/br/generic/notification/v2_0_0/BR-RemoveBusinessRegisterNotificationTemplate.xsd";
     public static final String CHANGE_BR_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-ChangeBusinessRegisterCodeNotificationTemplate.xsd";
-	//Legal Form Notification
+    //Legal Form Notification
     public static final String ADD_LEGAL_FORM_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-AddLegalFormNotificationTemplate.xsd";
-	public static final String REMOVE_LEGAL_FORM_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-RemoveLegalFormNotificationTemplate.xsd";
-	public static final String CHANGE_LEGAL_FORM_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-ChangeLegalFormNotificationTemplate.xsd";
-	//Change EUID Notification
+    public static final String REMOVE_LEGAL_FORM_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-RemoveLegalFormNotificationTemplate.xsd";
+    public static final String CHANGE_LEGAL_FORM_NOTIFICATION_SCHEMA = "xsd/bris/br/generic/notification/v2_0_0/BR-ChangeLegalFormNotificationTemplate.xsd";
+    //Change EUID Notification
     public static final String CHANGE_EUID_NOTIFICATION_SCHEMA ="xsd/bris/br/generic/notification/v2_0_0/BR-ChangeCompanyEUIDNotificationTemplate.xsd";
     //Business Register Acknowledgement
     public static final String ADD_BR_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-AddBusinessRegisterAcknowledgementTemplate.xsd";
     public static final String REMOVE_BR_ACKNOWLEDGEMENT_SCHEMA ="xsd/bris/br/generic/acknowledgement/v2_0_0/BR-RemoveBusinessRegisterAcknowledgementTemplate.xsd";
     public static final String CHANGE_BR_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-ChangeBusinessRegisterCodeAcknowledgementTemplate.xsd";
-	//Legal Form Acknowledgment
+    //Legal Form Acknowledgment
     public static final String ADD_LEGAL_FORM_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-AddLegalFormAcknowledgementTemplate.xsd";
-	public static final String REMOVE_LEGAL_FORM_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-RemoveLegalFormAcknowledgementTemplate.xsd";
-	public static final String CHANGE_LEGAL_FORM_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-ChangeLegalFormAcknowledgementTemplate.xsd";
-	//Change EUID Acknowledgment
+    public static final String REMOVE_LEGAL_FORM_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-RemoveLegalFormAcknowledgementTemplate.xsd";
+    public static final String CHANGE_LEGAL_FORM_ACKNOWLEDGEMENT_SCHEMA = "xsd/bris/br/generic/acknowledgement/v2_0_0/BR-ChangeLegalFormAcknowledgementTemplate.xsd";
+    //Change EUID Acknowledgment
     public static final String CHANGE_EUID_ACKNOWLEDGEMENT_SCHEMA ="xsd/bris/br/generic/acknowledgement/v2_0_0/BR-ChangeCompanyEUIDAcknowledgementTemplate.xsd";
-	
+
     private ResourcePathConstants() {
         // Hide constructor for utility class
     }
